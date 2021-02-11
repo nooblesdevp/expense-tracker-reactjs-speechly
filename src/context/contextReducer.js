aace16b8bf9930337c3a078e9aeefdc640b265d1
noobless <nuzulzen@gmail,com>
2021-02-11 10:55:47 +0700

    add localStorage.setItem

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -4,11 +4,14 @@ const contextReducer = (state, action) => {
   switch (action.type) {
     case "DELETE_TRANSACTION":
       transactions = state.filter((t) => t.id !== action.payload);
+      // add to save to localStorage
+      localStorage.setItem("transactions", JSON.stringify(transactions));
       return transactions;
 
     case "ADD_TRANSACTION":
       transactions = [action.payload, ...state];
-
+      // add to save to localStorage
+      localStorage.setItem("transactions", JSON.stringify(transactions));
       return transactions;
 
     default:
