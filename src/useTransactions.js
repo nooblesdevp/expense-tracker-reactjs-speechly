e71c0f2d5d7a2e25e1d061d8dda00d3b0ba03f4d
noobless <nuzulzen@gmail,com>
2021-02-11 10:17:07 +0700

    add functions hook transactions

---
+++ src/useTransactions.js
@@ -0,0 +1,42 @@
+import React, { useContext } from "react";
+import { ExpanseTrackerContext } from "./context/context";
+import {
+  incomeCategories,
+  expenseCategories,
+  resetCategories,
+} from "./constants/categories";
+
+const useTransactions = (title) => {
+  resetCategories();
+
+  const { transactions } = useContext(ExpanseTrackerContext);
+  const transactionsPerType = transactions.filter((t) => t.type === title);
+  const total = transactionsPerType.reduce(
+    (acc, currVal) => (acc += currVal.amount),
+    0
+  );
+  const categories = title === "Income" ? incomeCategories : expenseCategories;
+
+  console.log("yoo :>> ", { transactionsPerType, total, categories });
+
+  transactionsPerType.forEach((t) => {
+    const category = categories.find((c) => c.type === t.category);
+    // get type of transaction and added amount with categoriy
+    if (category) category.amount += t.amount;
+  });
+
+  const filteredCategories = categories.filter((c) => c.amount > 0);
+
+  const chartData = {
+    datasets: [
+      {
+        data: filteredCategories.map((c) => c.amount),
+        backgroundColor: filteredCategories.map((c) => c.color),
+      },
+    ],
+    labels: filteredCategories.map((c) => c.type),
+  };
+  return { total, chartData };
+};
+
+export default useTransactions;
