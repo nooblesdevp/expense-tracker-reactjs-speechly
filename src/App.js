0bfee30e7fd8dcc7862567f05cbf71eb94cd9956
noobless <nuzulzen@gmail,com>
2021-02-11 12:31:19 +0700

    fix mobile issue and add autoscroll when speechly active

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Details from "./components/Details/Details";
 import { Grid } from "@material-ui/core";
 import useStyles from "./styles";
@@ -9,8 +9,20 @@ import {
   PushToTalkButtonContainer,
 } from "@speechly/react-ui";
 
+import { SpeechState, useSpeechContext } from "@speechly/react-client";
+
 function App() {
   const classes = useStyles();
+
+  const executeScroll = () => main.current.scrollIntoView();
+  const main = useRef(null);
+  const { speechState } = useSpeechContext();
+
+  useEffect(() => {
+    if (speechState === SpeechState.Recording) {
+      executeScroll();
+    }
+  }, [speechState]);
 
   return (
     <div>
@@ -22,13 +34,16 @@ function App() {
         justify="center"
         style={{ height: "100vh" }}
       >
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={4} className={classes.mobile}>
           <Details title="Income" />
         </Grid>
-        <Grid item xs={12} sm={3}>
+        <Grid ref={main} item xs={12} sm={3} className={classes.main}>
           <Main />
         </Grid>
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={4} className={classes.desktop}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.last}>
           <Details title="Expense" />
         </Grid>
       </Grid>
