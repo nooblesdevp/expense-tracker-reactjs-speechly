7d1d1bbf91a9b402ac9ef9eea6168645e4f5a731
noobless <nuzulzen@gmail,com>
2021-02-11 10:19:31 +0700

    addd provider and speechly provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "./context/context";
+import { SpeechProvider } from "@speechly/react-client";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SpeechProvider
+      appId="c88120c9-a3bd-408d-96ca-4e4718e3d8a7"
+      language="en-US"
+    >
+      <Provider>
+        <App />
+      </Provider>
+    </SpeechProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
